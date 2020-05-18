290e9e9eb4fc106c1df4bfbe7b9e06235c459952
Javer Alcaraz <javi.alcaraz14@gmail.com>
2020-05-18 18:08:06 -0400

    intitial

---
+++ assets/main.js
@@ -0,0 +1,26 @@
+var dieRolls = []
+let rollButton = document.querySelector("#roll-button")
+let allButton = document.querySelector("#show-all-Button")
+
+
+rollButton.addEventListener("click", roll)
+function roll() {
+    var rollNumber = document.getElementById("roll-number").Value
+
+    console.log("roll was pressed!");
+}
+
+
+
+
+
+
+
+
+allButton.addEventListener("click", function () {
+
+})
+
+
+
+
